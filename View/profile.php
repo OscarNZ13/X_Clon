d946212da84516aeedd0aeef62f91535239e642c
OscarNZ13 <onaranjo00330@ufide.ac.cr>
2024-04-13 12:15:05 -0600

    Follower and followed
    Funcionan correctamente y en su debida pagina

--- View/profile.php
+++ View/profile.php
@@ -1,131 +1,166 @@
 <?php
-// Incluir el controlador del perfil
-include ('../Controller/profile_controller.php');
+session_start();
+include('../Db/Connection_db.php');
+include('../Controller/profile_controller.php');
+include_once('../Model/User_Model.php');
+include_once('../Model/Tweet_Model.php');
+
+if (isset($_SESSION['Usuario'])) {
+    $userModel_P = new UserModel();
+    $user = $userModel_P->getUserByUsername($username);
+    $userID_P = $user['ID_Usuario']; // Obtener el ID_Usuario del usuario logueado
+    $username = $user['Nombre'];
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
+    <!DOCTYPE html>
+    <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Perfil</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../Public/css/styles.css">
-    <link rel="stylesheet" href="../Public/css/sidebar.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mi Perfil</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../Public/css/styles.css">
+        <link rel="stylesheet" href="../Public/css/sidebar.css">
+    </head>
 
-<body>
-    <div class="container">
-        <div class="grid-container">
-            <div class="sidebar">
-                <ul style="list-style:none;">
-                    <li><i class="fa fa-twitter" style="color:#50b7f5;font-size:10px;"></i></li>
+    <body>
+        <div class="container">
+            <div class="grid-container">
+                <div class="sidebar">
+                    <ul style="list-style:none;">
+                        <li><i class="fa fa-twitter" style="color:#50b7f5;font-size:10px;"></i></li>
 
-                    <li class="active_menu"><a href='../View/home_page.php'><i class="fa fa-home"
-                                style="color:#50b7f5;"></i><span style="color:#50b7f5;">Home</span></a></li>
+                        <li class="active_menu"><a href='../View/home_page.php'><i class="fa fa-home" style="color:#50b7f5;"></i><span style="color:#50b7f5;">Home</span></a></li>
 
-                    <li><a href='#'><i class="fa fa-hashtag"></i><span>Explorar</span></a></li>
+                        <li><a href='#'><i class="fa fa-hashtag"></i><span>Explorar</span></a></li>
 
-                    <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i><span>Notificaciones</span>
+                        <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i><span>Notificaciones</span>
 
-                    <li id='messagePopup'><a><i class="fa fa-envelope" aria-hidden='true'></i><span>Mensajes</span>
+                        <li id='messagePopup'><a><i class="fa fa-envelope" aria-hidden='true'></i><span>Mensajes</span>
 
-                    <li><a href='../View/profile.php?username=<?php echo $_SESSION['Usuario']; ?>' class="Username-link"><i class="fa fa-user"></i> Perfil</a></li>
+                        <li><a href='../View/profile.php?username=<?php echo $_SESSION['Usuario']; ?>' class="Username-link"><i class="fa fa-user"></i> Perfil</a></li>
 
-                    <li><a href='../View/profile_edit.php'><i class="fa fa-cog"></i><span>Editar Perfil</span></a></li>
-                    <li>
-                        <form action="../Controller/logout_controller.php" method="post">
-                            <button type="submit" class="logout-btn">
-                                <i class="fa fa-power-off"></i>
-                                <span>Cerrar Sesión</span>
-                            </button>
-                        </form>
-                    </li>
+                        <?php if (isset($_SESSION['Usuario']) == $username) { ?>
+                            <li><a href='../View/profile_edit.php'><i class="fa fa-cog"></i><span>Editar Perfil</span></a></li>
 
-                    <li id='tweetButton'><button class="sidebar_tweet button addTweetBtn"
-                            style="outline:none;">Tweet</button></li>
-                    <li id="tweetBox" style="display:none;">
-                        <form action="../Controller/tweet_controller.php" method="post" class="tweet-form">
-                            <textarea name="contenido" placeholder="Escribe tu tweet aquí" required></textarea>
-                            <button type="submit">Enviar</button>
-                        </form>
-                    </li>
+                            <li>
+                                <form action="../Controller/logout_controller.php" method="post">
+                                    <button type="submit" class="logout-btn">
+                                        <i class="fa fa-power-off"></i>
+                                        <span>Cerrar Sesión</span>
+                                    </button>
+                                </form>
+                            </li>
 
-                </ul>
-            </div>
-            <div class="main">
-                <div class="profile">
-                    <?php if ($user): ?>
-                        <div class="profile-info">
-                            <img src="<?php echo $user['FotoPerfil']; ?>" alt="Foto de perfil">
-                            <h1><?php echo $user['Nombre']; ?></h1>
-                            <p>Descripción: <?php echo $user['Biografia']; ?></p>
-                            <p>Ubicación: <?php echo $user['Ubicacion']; ?></p>
-                            <form method="POST">
-                                <button type="submit" name="follow" class="leave-btn">Seguir</button>
-                                <button type="submit" name="message" class="leave-btn">Mensaje</button>
-                            </form>
-                        </div>
-                        <div class="tweets">
-                            <h2>Mis Tweets</h2>
-                            <?php foreach ($tweets as $tweet): ?>
-                                <div class="tweet">
-                                    <p><?php echo $tweet['Contenido']; ?></p>
-                                    <span><?php echo $tweet['FechaPublicacion']; ?></span>
-                                    <div class="tweet-actions">
-                                        <button class="like-btn likes-count"> <?php echo $tweet['Likes']; ?> Like</button>
-                                        <button class="comment-btn comments-count"><?php echo $tweet['Retweets']; ?>
-                                            Comentar</button>
+                            <li id='tweetButton'><button class="sidebar_tweet button addTweetBtn" style="outline:none;">Tweet</button></li>
+
+                            <li id="tweetBox" style="display:none;">
+                                <form action="../Controller/tweet_controller.php" method="post" class="tweet-form">
+                                    <textarea name="contenido" placeholder="Escribe tu tweet aquí" required></textarea>
+                                    <button type="submit">Enviar</button>
+                                </form>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+                <div class="main">
+                    <div class="profile">
+                        <?php if ($user) : ?>
+                            <div class="profile-info">
+                                <img src="<?php echo $user['FotoPerfil']; ?>" alt="Foto de perfil">
+                                <h1><?php echo $user['Nombre']; ?></h1>
+                                <p>Descripción: <?php echo $user['Biografia']; ?></p>
+                                <p>Ubicación: <?php echo $user['Ubicacion']; ?></p>
+                                <?php if (isset($_SESSION['Usuario']) != $username) { ?>
+                                    <form action="" method="POST">
+                                        <button type="submit" name="follow" class="leave-btn">Seguir</button>
+                                    </form>
+                                <?php } ?>
+                            </div>
+                            <div class="tweets">
+                                <h2>Mis Tweets</h2>
+                                <?php
+                                // Consulta SQL para recuperar los tweets
+                                $tweetModel_P = new TweetModel();
+                                $tweets = $tweetModel_P->getTweetsForUser($userID_P);
+
+                                foreach ($tweets as $tweet) :
+                                    $tweetID = $tweet['ID_Tweet'];
+                                    $tweetUserID_P = $tweet['ID_Usuario']; ?>
+
+                                    <div class="tweet">
+                                        <p><?php echo $tweet['Contenido']; ?></p>
+                                        <span><?php echo $tweet['FechaPublicacion']; ?></span>
+                                        <div class="tweet-actions">
+
+                                            <?php if ($tweetUserID_P === $userID_P) { // Verificar si el usuario logeado es el propietario del tweet
+                                                echo '<form action="../Controller/tweet_controller.php" method="post">';
+                                                echo '<input type="hidden" name="tweet_id" value="' . $tweetID . '">';
+                                                echo '<button type="submit" name="delete_tweet">Eliminar</button>';
+                                                echo '</form>';
+                                            }
+                                            ?>
+                                            <button class="like-btn likes-count"> <?php echo $tweet['Likes']; ?> Like</button>
+                                            <button class="comment-btn comments-count"><?php echo $tweet['Retweets']; ?>
+                                                Comentar</button>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
-            </div>
-            <div class="right_sidebar">
-                <h2>Tendencias</h2>
-                <!-- Aquí puedes incluir contenido dinámico de tendencias -->
-                <div class="trend">
-                    <p>#ElonMusk</p>
-                </div>
-                <div class="trend">
-                    <p>#CostaRica</p>
-                </div>
-                <div class="trend">
-                    <p>#UFidélitas</p>
-                </div>
+                <div class="right_sidebar">
+                    <h2>Tendencias</h2>
+                    <!-- Aquí puedes incluir contenido dinámico de tendencias -->
+                    <div class="trend">
+                        <p>#ElonMusk</p>
+                    </div>
+                    <div class="trend">
+                        <p>#CostaRica</p>
+                    </div>
+                    <div class="trend">
+                        <p>#UFidélitas</p>
+                    </div>
 
-                <h2>Personas para seguir</h2>
-                <!-- Aquí puedes incluir contenido dinámico de personas para seguir -->
-                <div class="person-to-follow">
-                    <img src="https://this-person-does-not-exist.com/img/avatar-gen11a51f475a14d52c0afabe1b9cdd0ff2.jpg" alt="Avatar">
-                    <p>Luis Alfonso Puertas</p>
-                    <button class="follow-btn">Seguir</button>
-                </div>
-                <div class="person-to-follow">
-                    <img src="https://this-person-does-not-exist.com/img/avatar-gen78c54f6dec142ac4c33c14fe035d18f6.jpg" alt="Avatar">
-                    <p>Vanessa Blazquez</p>
-                    <button class="follow-btn">Seguir</button>
+                    <h2>Personas para seguir</h2>
+                    <!-- Aquí puedes incluir contenido dinámico de personas para seguir -->
+                    <div class="person-to-follow">
+                        <img src="https://this-person-does-not-exist.com/img/avatar-gen11a51f475a14d52c0afabe1b9cdd0ff2.jpg" alt="Avatar">
+                        <p>Luis Alfonso Puertas</p>
+                        <button class="follow-btn">Seguir</button>
+                    </div>
+                    <div class="person-to-follow">
+                        <img src="https://this-person-does-not-exist.com/img/avatar-gen78c54f6dec142ac4c33c14fe035d18f6.jpg" alt="Avatar">
+                        <p>Vanessa Blazquez</p>
+                        <button class="follow-btn">Seguir</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const tweetButton = document.getElementById('tweetButton');
-            const tweetBox = document.getElementById('tweetBox');
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const tweetButton = document.getElementById('tweetButton');
+                const tweetBox = document.getElementById('tweetBox');
 
-            tweetButton.addEventListener('click', function () {
-                if (tweetBox.style.display === 'none' || tweetBox.style.display === '') {
-                    tweetBox.style.display = 'block';
-                } else {
-                    tweetBox.style.display = 'none';
-                }
+                tweetButton.addEventListener('click', function() {
+                    if (tweetBox.style.display === 'none' || tweetBox.style.display === '') {
+                        tweetBox.style.display = 'block';
+                    } else {
+                        tweetBox.style.display = 'none';
+                    }
+                });
             });
-        });
-    </script>
-</body>
+        </script>
+    </body>
 
-</html>
+    </html>
+
+
+<?php
+} else {
+    header("Location: ../View/index.php");
+    exit();
+}
+?>
--- View/profile_edit.php
+++ View/profile_edit.php
@@ -1,136 +1,136 @@
 <?php
 // Incluir el controlador del perfil
-include ('../Controller/profile_edit_controller.php');
-?>
+include('../Controller/profile_edit_controller.php');
+if (isset($_SESSION['Usuario'])) { ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Profile</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../Public/css/sidebar.css">
-    <link rel="stylesheet" href="../Public/css/styles.css">
-    <link rel="stylesheet" href="../Public/css/ProfileEdit_style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edit Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../Public/css/sidebar.css">
+        <link rel="stylesheet" href="../Public/css/styles.css">
+        <link rel="stylesheet" href="../Public/css/ProfileEdit_style.css">
+    </head>
 
-<body>
-    <div class="container">
-        <div class="grid-container">
-        <div class="sidebar">
-                <ul style="list-style:none;">
-                    <li><i class="fa fa-twitter" style="color:#50b7f5;font-size:10px;"></i></li>
+    <body>
+        <div class="container">
+            <div class="grid-container">
+                <div class="sidebar">
+                    <ul style="list-style:none;">
+                        <li><i class="fa fa-twitter" style="color:#50b7f5;font-size:10px;"></i></li>
 
-                    <li class="active_menu"><a href='../View/home_page.php'><i class="fa fa-home"
-                                style="color:#50b7f5;"></i><span style="color:#50b7f5;">Home</span></a></li>
+                        <li class="active_menu"><a href='../View/home_page.php'><i class="fa fa-home" style="color:#50b7f5;"></i><span style="color:#50b7f5;">Home</span></a></li>
 
-                    <li><a href='#'><i class="fa fa-hashtag"></i><span>Explorar</span></a></li>
+                        <li><a href='#'><i class="fa fa-hashtag"></i><span>Explorar</span></a></li>
 
-                    <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i><span>Notificaciones</span>
+                        <li><a href="#"><i class="fa fa-bell" aria-hidden="true"></i><span>Notificaciones</span>
 
-                    <li id='messagePopup'><a><i class="fa fa-envelope" aria-hidden='true'></i><span>Mensajes</span>
+                        <li id='messagePopup'><a><i class="fa fa-envelope" aria-hidden='true'></i><span>Mensajes</span>
 
-                    <li><a href='../View/profile.php?username=<?php echo $_SESSION['Usuario']; ?>' class="Username-link"><i class="fa fa-user"></i> Perfil</a></li>
+                        <li><a href='../View/profile.php?username=<?php echo $_SESSION['Usuario']; ?>' class="Username-link"><i class="fa fa-user"></i> Perfil</a></li>
 
-                    <li><a href='../View/profile_edit.php'><i class="fa fa-cog"></i><span>Editar Perfil</span></a></li>
-                    <li>
-                        <form action="../Controller/logout_controller.php" method="post">
-                            <button type="submit" class="logout-btn">
-                                <i class="fa fa-power-off"></i>
-                                <span>Cerrar Sesión</span>
-                            </button>
+                        <li><a href='../View/profile_edit.php'><i class="fa fa-cog"></i><span>Editar Perfil</span></a></li>
+                        <li>
+                            <form action="../Controller/logout_controller.php" method="post">
+                                <button type="submit" class="logout-btn">
+                                    <i class="fa fa-power-off"></i>
+                                    <span>Cerrar Sesión</span>
+                                </button>
+                            </form>
+                        </li>
+
+                        <li id='tweetButton'><button class="sidebar_tweet button addTweetBtn" style="outline:none;">Tweet</button></li>
+                        <li id="tweetBox" style="display:none;">
+                            <form action="../Controller/tweet_controller.php" method="post" class="tweet-form">
+                                <textarea name="contenido" placeholder="Escribe tu tweet aquí" required></textarea>
+                                <button type="submit">Enviar</button>
+                            </form>
+                        </li>
+
+                    </ul>
+                </div>
+                <div class="main">
+                    <div class="profile-edit">
+                        <h1>Edit Profile</h1>
+                        <form action="../Controller/profile_edit_controller.php" method="POST">
+                            <div class="form-group">
+                                <label for="name">Nombre:</label>
+                                <input type="text" id="name" name="name" value="<?php echo $user['Nombre']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" id="email" name="email" value="<?php echo $user['CorreoElectronico']; ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="bio">Descripción:</label>
+                                <textarea id="bio" name="bio"><?php echo $user['Biografia']; ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="location">Ubicación:</label>
+                                <input type="text" id="location" name="location" value="<?php echo $user['Ubicacion']; ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="profile-pic">Foto de Perfil:</label>
+                                <input type="text" id="profile-pic" name="profile-pic" value="<?php echo $user['FotoPerfil']; ?>">
+                            </div>
+                            <button type="submit" name="save_changes" value="Save Changes" class="btn-profile">Guardar cambios</button>
                         </form>
-                    </li>
-
-                    <li id='tweetButton'><button class="sidebar_tweet button addTweetBtn"
-                            style="outline:none;">Tweet</button></li>
-                    <li id="tweetBox" style="display:none;">
-                        <form action="../Controller/tweet_controller.php" method="post" class="tweet-form">
-                            <textarea name="contenido" placeholder="Escribe tu tweet aquí" required></textarea>
-                            <button type="submit">Enviar</button>
-                        </form>
-                    </li>
-
-                </ul>
-            </div>
-            <div class="main">
-                <div class="profile-edit">
-                    <h1>Edit Profile</h1>
-                    <form action="../Controller/profile_edit_controller.php" method="POST">
-                        <div class="form-group">
-                            <label for="name">Nombre:</label>
-                            <input type="text" id="name" name="name" value="<?php echo $user['Nombre']; ?>" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" id="email" name="email"
-                                value="<?php echo $user['CorreoElectronico']; ?>" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="bio">Descripción:</label>
-                            <textarea id="bio" name="bio"><?php echo $user['Biografia']; ?></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="location">Ubicación:</label>
-                            <input type="text" id="location" name="location" value="<?php echo $user['Ubicacion']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="profile-pic">Foto de Perfil:</label>
-                            <input type="text" id="profile-pic" name="profile-pic"
-                                value="<?php echo $user['FotoPerfil']; ?>">
-                        </div>
-                        <button type="submit" name="save_changes" value="Save Changes" class="btn-profile">Guardar cambios</button>
-                    </form>
-                </div>
-            </div>
-
-            <div class="right_sidebar">
-                <h2>Tendencias</h2>
-                <!-- Aquí puedes incluir contenido dinámico de tendencias -->
-                <div class="trend">
-                    <p>#ElonMusk</p>
-                </div>
-                <div class="trend">
-                    <p>#CostaRica</p>
-                </div>
-                <div class="trend">
-                    <p>#UFidélitas</p>
+                    </div>
                 </div>
 
-                <h2>Personas para seguir</h2>
-                <!-- Aquí puedes incluir contenido dinámico de personas para seguir -->
-                <div class="person-to-follow">
-                    <img src="https://this-person-does-not-exist.com/img/avatar-gen11a51f475a14d52c0afabe1b9cdd0ff2.jpg"
-                        alt="Avatar">
-                    <p>Luis Alfonso Puertas</p>
-                    <button class="follow-btn">Seguir</button>
-                </div>
-                <div class="person-to-follow">
-                    <img src="https://this-person-does-not-exist.com/img/avatar-gen78c54f6dec142ac4c33c14fe035d18f6.jpg"
-                        alt="Avatar">
-                    <p>Vanessa Blazquez</p>
-                    <button class="follow-btn">Seguir</button>
+                <div class="right_sidebar">
+                    <h2>Tendencias</h2>
+                    <!-- Aquí puedes incluir contenido dinámico de tendencias -->
+                    <div class="trend">
+                        <p>#ElonMusk</p>
+                    </div>
+                    <div class="trend">
+                        <p>#CostaRica</p>
+                    </div>
+                    <div class="trend">
+                        <p>#UFidélitas</p>
+                    </div>
+
+                    <h2>Personas para seguir</h2>
+                    <!-- Aquí puedes incluir contenido dinámico de personas para seguir -->
+                    <div class="person-to-follow">
+                        <img src="https://this-person-does-not-exist.com/img/avatar-gen11a51f475a14d52c0afabe1b9cdd0ff2.jpg" alt="Avatar">
+                        <p>Luis Alfonso Puertas</p>
+                        <button class="follow-btn">Seguir</button>
+                    </div>
+                    <div class="person-to-follow">
+                        <img src="https://this-person-does-not-exist.com/img/avatar-gen78c54f6dec142ac4c33c14fe035d18f6.jpg" alt="Avatar">
+                        <p>Vanessa Blazquez</p>
+                        <button class="follow-btn">Seguir</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const tweetButton = document.getElementById('tweetButton');
-            const tweetBox = document.getElementById('tweetBox');
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const tweetButton = document.getElementById('tweetButton');
+                const tweetBox = document.getElementById('tweetBox');
 
-            tweetButton.addEventListener('click', function () {
-                if (tweetBox.style.display === 'none' || tweetBox.style.display === '') {
-                    tweetBox.style.display = 'block';
-                } else {
-                    tweetBox.style.display = 'none';
-                }
+                tweetButton.addEventListener('click', function() {
+                    if (tweetBox.style.display === 'none' || tweetBox.style.display === '') {
+                        tweetBox.style.display = 'block';
+                    } else {
+                        tweetBox.style.display = 'none';
+                    }
+                });
             });
-        });
-    </script>
-</body>
+        </script>
+    </body>
 
-</html>
+    </html>
+<?php
+} else {
+    header("Location: ../View/index.php");
+    exit();
+}
+?>
